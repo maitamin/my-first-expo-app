58e9e3032cc288ac397ee09ec8a2c25d3fcdf2b8
maitamin <3617510+maitamin@users.noreply.github.com>
2024-12-03 08:38:00 +0900

    第2回 リスト2

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
       <StatusBar style="auto" />
     </View>
   );
